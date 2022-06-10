1ca704274fe03b3e6099a8d4204d2e0a21cad02d
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2022-06-10 13:59:10 -0400

    Implement currency conversion without fees attached

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -31,53 +31,62 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // include api for currency change
+    const api = "https://api.exchangerate-api.com/v4/latest/USD";
+
+    // for selecting different controls
+    var search = document.querySelector(".searchBox");
+    var convert = document.querySelector(".convert");
+    var fromCurrency = document.querySelector(".from");
+    var toCurrency = document.querySelector(".to");
+    var finalValue = document.querySelector(".finalValue");
+    var finalAmount = document.getElementById("finalAmount");
+    var resultFrom;
+    var resultTo;
+    var searchValue;
+
+    // Event when currency is changed
+    fromCurrency.addEventListener('change', (event) => {
+    	resultFrom = `${event.target.value}`;
+    });
+
+    // Event when currency is changed
+    toCurrency.addEventListener('change', (event) => {
+    	resultTo = `${event.target.value}`;
+    });
+
+    search.addEventListener('input', updateValue);
+
+    // function for updating value
+    function updateValue(e) {
+    	searchValue = e.target.value;
+    }
+
+    // when user clicks, it calls function getresults
+    convert.addEventListener("click", getResults);
+
+    // function getresults
+    function getResults() {
+    	fetch(`${api}`)
+    		.then(currency => {
+    			return currency.json();
+    		}).then(displayResults);
+    }
+
+    // display results after convertion
+    function displayResults(currency) {
+    	let fromRate = currency.rates[resultFrom];
+    	let toRate = currency.rates[resultTo];
+    	finalValue.innerHTML =
+    	((toRate / fromRate) * searchValue).toFixed(2);
+    	finalAmount.style.display = "block";
+    }
+
 });
 
-const select = document.querySelectorAll('.currency');
-    const number = document.getElementById("number");
-    const output = document.getElementById("output");
-
-
-    fetch('https://api.frankfurter.app/currencies').then((data) => data.json())
-      .then((data) => {
-        display(data);
-      });
-
-
-    function display(data) {
-      const entries = Object.entries(data);
-      for (var i = 0; i < entries.length; i++) {
-        select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
-        select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
-      }
-    }
-
-
-
-    function updatevalue() {
-      let currency1 = select[0].value;
-      let currency2 = select[1].value;
-
-      let value = number.value;
-
-
-      if (currency1 != currency2) {
-        convert(currency1, currency2, value);
-      } else {
-        alert("Choose Diffrent Currency");
-      }
-    }
-
-
-    function convert(currency1, currency2, value) {
-      const host = "api.frankfurter.app";
-
-      fetch(`https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`)
-        .then((val) => val.json())
-        .then((val) => {
-          console.log(Object.values(val.rates)[0]);
-          output.value = Object.values(val.rates)[0];
-        });
-
-    }
+// when user click on reset button
+    function clearVal() {
+    	window.location.reload();
+    	document.getElementsByClassName("finalValue").innerHTML = "";
+    };
 
